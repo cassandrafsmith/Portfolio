5799dafb2608e62ec1b35c02579d3e3767c48b41
Cassandra Smith <65835968+cassandrafsmith@users.noreply.github.com>
2023-01-10 23:00:11 -0600

    set initial layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,23 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Open Sans`,
+            file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap`,
+          },
+          {
+            name: `Raleway`,
+            file: `https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap`,
+          },
+        ],
+      },
+    },
+  ],
 }
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const Contact = () => {
+    return(
+        <div>
+            <h1>Hello!</h1>
+            <p>You can reach me @CassandraFSmith </p>
+           <Link to= '/'> Home </Link>
+        </div>       
+    )
+}
+
+export default Contact;
