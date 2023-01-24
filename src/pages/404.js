a71f1975326775a824af01acb199de093cde7473
Cassandra Smith <65835968+cassandrafsmith@users.noreply.github.com>
2023-01-23 19:44:35 -0600

    format css for for-submission page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
+
 
 const pageStyles = {
   color: "#232129",
--- src/pages/form-submission.js
+++ src/pages/form-submission.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { Link } from 'gatsby';
 import image from './../images/favicon/favicon-32x32.png';
+import './../styles/form-submission.css';
+import close from './../images/close_FILL0_wght400_GRAD0_opsz48.svg'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const FormSubmission = () => {
+    useEffect(() => {
+        AOS.init({
+            duration: 1200,
+            easing: 'ease-in-out-back',
+            once: true,       
+        });
+      }, []);
+
     return(
-        <div>
-            <h1>Thank You for Your Submission!</h1>
-            <p>I appreciate you reaching out and I will be in touch with you soon! 😀</p>
-            <Link to='/index' ><img src={image} alt='back' /></Link>
+        <div className='fs-main' >
+            <Link to='/' id='close' ><img src={close} alt='back' /></Link>
+            <div>
+                <h1 id='fs-title' data-aos="zoom-in-right" >Thank You for Your Submission!</h1>
+                <h3 id='fs-copy' data-aos="zoom-in-left" data-aos-duration='3000' >
+                    I appreciate you reaching out and 
+                    I will be in touch with you soon! 😀
+                </h3>
+            </div>
         </div>
     )
 };
 
 export default FormSubmission;
+
+export { Head } from "./index";
