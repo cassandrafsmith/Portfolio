b4f811d650c45e4f26d4a81fe68d3287e12c1dce
Cassandra Smith <65835968+cassandrafsmith@users.noreply.github.com>
2023-01-29 21:00:38 -0600

    add labyrinth game page

--- src/assets/projectCardInfo.js
+++ src/assets/projectCardInfo.js
@@ -1,6 +1,9 @@
 //object to store project information
 
 import mockupOne from './../images/DT_mockup.svg';
+import dtMockup352 from './../images/mockups/DT_mockup352.svg';
+import labyrinthMockup352 from './../images/mockups/labyrinth_mockup352.svg';
+import portfolioMockup352 from './../images/mockups/portfolio_mockup352.svg';
 
 export const projectCardInfo =[
     {      
@@ -14,32 +17,28 @@ export const projectCardInfo =[
         tools: 'React • ReactRouter • Node.js • Express • PostgreSQL',
         github: 'https://github.com/KSU-Capstone-22/decentralized-tech-website',
         url: 'https://decentralizedtech.org/',
-        mockup: mockupOne,
+        mockup: dtMockup352,
     },
     {       
-        title: 'Website Build',
-        description: `For this project I, along with my
-        capstone team members, built a complete website
-        rebuild for Decentralized Technologies, a 
-        company located in Merietta, GA. The build included the 
-        public facing website, along with admin, 
-        employee, and client dashboards.`,
-        tools: 'React • Node.js • Express • PostgreSQL',
-        github: 'https://github.com/KSU-Capstone-22/decentralized-tech-website',
-        url: 'https://decentralizedtech.org/',
-        mockup: mockupOne,    
+        title: 'Labyrinth Game',
+        description: `For this project, I lead a group of
+         final semester team mates in the planning, design, 
+         and building of a labyrinth puzzle game using the 
+         Unity Game Engine.`,
+        tools: 'C# • Unity Game Engine ',
+        github: 'https://github.com/cassandrafsmith/labyrinth-game',
+        url: 'https://cassandrafsmith.itch.io/labyrinth',
+        mockup: labyrinthMockup352,    
     },
     {       
-        title: 'Website Build3',
-        description: `For this project I, along with my
-        capstone team members, built a complete website
-        rebuild for Decentralized Technologies, a 
-        company located in Merietta, GA. The build included the 
-        public facing website, along with admin, 
-        employee, and client dashboards.`,
-        tools: 'React • Node.js • Express • PostgreSQL',
-        github: 'https://github.com/KSU-Capstone-22/decentralized-tech-website',
-        url: 'https://decentralizedtech.org/',
-        mockup: mockupOne,    
+        title: 'Personal Portfolio',
+        description: `For this project I built my personal
+        portfolio using React. It was an exciting project to design 
+        and built because it gave me the opportunity 
+        to learn about and work with the Gatsby framework.`,
+        tools: 'CSS • React • Gatsby',
+        github: 'https://github.com/cassandrafsmith/Portfolio',
+        url: 'https://cassandrafsmith.com/',
+        mockup: portfolioMockup352,    
     },
 ];
